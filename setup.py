e07ab051c779b450d708835c60fab992557b05de
Alexander Myronov <oleksandr.myronov@ardigen.com>
2017-10-19 19:21:57 +0200

    0.2 version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bayesian',
-      version='0.1',
+      version='0.2',
       description='A collection of utils and examples on Bayesian learning',
       author='Oleksandr Myronov',
       author_email='oleksandr.myronov@ardigen.com',
