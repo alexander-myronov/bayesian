168d22f6330f4130864bc5df74c2e2ef8918b25a
Alexander Myronov <oleksandr.myronov@ardigen.com>
2017-10-20 10:09:37 +0200

    setup.py: package versions added

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ setup(name='bayesian',
       author='Oleksandr Myronov',
       author_email='oleksandr.myronov@ardigen.com',
       packages=['src/bayesian'],
-      install_requires=['keras', 'numpy', 'scipy'],
+      install_requires=[
+          'keras>=2',
+          'numpy>=1.11',
+          'scipy>=0.19'
+      ],
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
       zip_safe=False)
